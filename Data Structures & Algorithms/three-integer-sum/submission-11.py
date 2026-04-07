63a3455d1c8f77b0687aa5e89a64a9d6bd7f85a9
Ali Asghar <98263017+alliasgher@users.noreply.github.com>
2026-04-07 20:34:38 +0500

    Add: three-integer-sum - submission-11

---
+++ Data Structures & Algorithms/three-integer-sum/submission-11.py
@@ -0,0 +1,60 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # nums.sort()
+        # res = []
+
+        # for i, num in enumerate(nums):
+
+        #     if i>0 and num==nums[i-1]:
+        #         continue
+            
+        #     l,r = i+1, len(nums)-1
+
+        #     while l<r:
+        #         summ = num + nums[l] + nums[r]
+        #         if summ > 0:
+        #             r -= 1
+        #         elif summ <0:
+        #             l+= 1
+        #         else:
+        #             res.append([num, nums[l], nums[r]])
+        #             l += 1
+        #             r -= 1
+        #             while l < r and nums[l] == nums[l-1]:
+        #                 l += 1
+        #             while l < r and nums[r] == nums[r+1]:
+        #                 r -= 1
+        # return res
+
+        nums.sort()
+        res = []
+
+        for i in range(len(nums)):
+
+            if i> 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            tmp = 0 - nums[i]
+
+            while l<r:
+
+                if nums[l] + nums[r] < tmp:
+                    l += 1
+                elif nums[l] + nums[r] > tmp:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                
+                    while l < r and nums[l] == nums[l+1]:
+                        l+= 1
+                    while l<r and nums[r] == nums[r-1]:
+                        r-= 1
+                    
+                    l+= 1
+                    r-= 1
+
+        return res
+                    
+
+
+        
